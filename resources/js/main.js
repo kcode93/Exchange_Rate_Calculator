73efb2e2ce21b229de5b06bd5dadc9c406655799
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-05-14 15:47:39 -0700

    if applied, this commit selects the required elements, and adds initial test function

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,24 @@
-//Scripts Here
+//Target elements
+const currencyElementOne = document.querySelector('#currency-one');
+const currencyElementOneAmnt = document.querySelector('#amount-one');
+const currencyElementTwo = document.querySelector('#currency-two');
+const currencyElementTwoAmnt = document.querySelector('#amount-two');
+const rate = document.querySelector('#rate');
+const swap_btn = document.querySelector('#swap');
+let counter = 0;
+
+
+//Exchange Rates & Update DOM
 function calculate (){
-    fetch('items.json').then(function(res){
-        console.log(res);
-    });
+   console.log(`Hello MTF ${counter}`);
+   counter++;
 }
 
-calculate();
+//Event Listeners
+currencyElementOne.addEventListener('change', calculate);
+currencyElementOneAmnt.addEventListener('input', calculate);
+currencyElementTwo.addEventListener('change', calculate);
+currencyElementTwoAmnt.addEventListener('input', calculate);
+
+//Run Function On Startup
+window.addEventListener('DOMContentLoaded', calculate);
