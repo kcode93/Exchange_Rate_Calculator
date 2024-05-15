5faca60580547643a425b3ffb127075312b31cec
Kenneth Garcia <kennethgarcia12@hotmail.com>
2024-05-15 15:49:26 -0700

    if applied, this commit adds the swapCurrency function and swap_btn event listener to swap the currency type source(s)

--- resources/js/main.js
+++ resources/js/main.js
@@ -16,7 +16,7 @@ function calculate (){
    fetch(`https://open.er-api.com/v6/latest/${currOne_val}`)
    .then(res => res.json())
    .then(data => {
-        let localRate = data.rates[currTwo_val];
+        let localRate = data.rates[currTwo_val].toFixed(4);
         ratebox.innerText = `1 ${currOne_val} = ${localRate} ${currTwo_val}`;
 
         currencyElementTwoAmnt.value = (currencyElementOneAmnt.value * localRate).toFixed(2);
@@ -25,11 +25,20 @@ function calculate (){
    
 }
 
+//Swaps the Currency Source
+function swapCurrency(){
+   const temp = currencyElementOne.value;
+   currencyElementOne.value = currencyElementTwo.value;
+   currencyElementTwo.value = temp;
+   calculate();
+}
+
 //Event Listeners
 currencyElementOne.addEventListener('change', calculate);
 currencyElementOneAmnt.addEventListener('input', calculate);
 currencyElementTwo.addEventListener('change', calculate);
 currencyElementTwoAmnt.addEventListener('input', calculate);
+swap_btn.addEventListener('click', swapCurrency);
 
 //Run Function On Startup
 window.addEventListener('DOMContentLoaded', calculate);
